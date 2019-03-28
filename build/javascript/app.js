7346fe24527ef53b3827c76ff77df5c9f351f6d8
Sam Dantas <contato.ashanti@gmail.com>
2019-03-28 12:41:17 -0300

    Added Temperature Toggler

--- build/javascript/app.js
+++ build/javascript/app.js
@@ -4,8 +4,9 @@ window.addEventListener('DOMContentLoaded', () => {
   const tempDescription = document.querySelector('.temp-description');
   const tempDegree = document.querySelector('.temp-degree');
   const locLocation = document.querySelector('.loc-location');
-  //Global Variables
+
   let noDecision = true;
+  let temp;
 
   //If no decision advert user
   setTimeout(() => {
@@ -13,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
       tempDescription.innerHTML = 'Please click on allow location to see the current weather of your city!';
     }
   }, 5000)
-  //Functions
+  // Success Function --------------------------------
   const success = (pos) => {
     noDecision = false;
 
@@ -35,6 +36,8 @@ window.addEventListener('DOMContentLoaded', () => {
       });
   }
 
+  // Error Function --------------------------------
+  //If geolocation is not allowed advert user
   const error = () => {
     noDecision = false;
     tempDescription.innerHTML = 'For this app work properly, you need to allow your location access!';
@@ -42,16 +45,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const dataToUser = (data) => {
     console.log(data);
-    tempDegree.innerHTML = data.currently.temperature;
+    temp = data.currently.temperature;
+    temperatureScaleToggler(temp);
     tempDescription.innerHTML = data.hourly.summary;
     locLocation.innerHTML = data.timezone;
   }
 
+  //Toggles scale between C and F
+  
+  let tempIndex = 1;
+
+  const temperatureScaleToggler = (temp) => {
+    if(tempIndex % 2 == 0) {
+      const f = temp.toFixed(1);
+      //Show current Temperature in Fahrenheit
+      return tempDegree.innerHTML = `${f} F`;
+    } else {
+      const c = ((temp - 32) * 5/9).toFixed(1);
+      //Transforms and show temperature in Celsius
+      return tempDegree.innerHTML = `${c}&#176 C`;
+    }
+  }
+
+  document.addEventListener('click', () => {
+    tempIndex++;
+    temperatureScaleToggler(temp);
+  });
+
+
 
 
   navigator.geolocation.getCurrentPosition(success, error);
-  //If geolocation is not allowed advert user
 
 }); //___DOMContentLoaded
-//Show current degree in celcius and Fahrenheit
 //Show current city location

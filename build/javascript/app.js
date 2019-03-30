4a89fa4cffa7fc21b15bd35130a5830c659abd54
Sam Dantas <contato.ashanti@gmail.com>
2019-03-29 22:28:11 -0300

    Added skycons, better icon responsiveness and a more readable code

--- build/javascript/app.js
+++ build/javascript/app.js
@@ -4,94 +4,110 @@ window.addEventListener('DOMContentLoaded', () => {
   const tempDescription = document.querySelector('.temp-description');
   const tempDegree = document.querySelector('.temp-degree');
   const locLocation = document.querySelector('.loc-location');
-  const tempPrecipitation = document.querySelector('.temp-precipitation')
-  
+  const tempPrecipitation = document.querySelector('.temp-precipitation');
+
   let noDecision = true;
   let temp;
 
   //If no decision advert user
   setTimeout(() => {
-    if(noDecision) {
-      tempDescription.innerHTML = 'Please click on allow location to see the current weather of your city!';
+    if (noDecision) {
+      tempDescription.innerHTML =
+        'Please click on allow location to see the current weather of your city!';
     }
-  }, 5000)
+  }, 5000);
+
+  // Add Default Icon by Skyconjs
+  const icon = new Skycons({color: '#ededed'});
+  icon.add('weather-icon', Skycons.PARTLY_CLOUDY_DAY);
+
   // Success Function --------------------------------
-  const success = (pos) => {
+  const success = pos => {
     noDecision = false;
 
-    let crd = pos.coords; 
+    let crd = pos.coords;
     let lat = crd.latitude;
     let lng = crd.longitude;
 
-    //Here API (reverse geolocation) needs an appID and a appCode key
-    const appID = 'QADFRxDFnnKNTH6AxKQN';
-    const appCode = 'UPX7SLsw_SDme3BEECAfWQ';
+    //Here API (reverse geolocation) needs an hereID and a hereAppCode key
+    const hereID = 'QADFRxDFnnKNTH6AxKQN';
+    const hereAppCode = 'UPX7SLsw_SDme3BEECAfWQ';
+    const darkSkyKey = '1c9aa3f4a39a987be8e736bd3a9764e1';
 
-    
     //Quick solve for the cross origin problem
     const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const weatherAPI = `${proxy}https://api.darksky.net/forecast/1c9aa3f4a39a987be8e736bd3a9764e1/${lat},${lng}`;
-    const locationAPI = `${proxy}https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?prox=${lat},${lng}&mode=retrieveAddresses&maxresults=1&app_id=${appID}&app_code=${appCode}`;
+    const weatherAPI = `${proxy}https://api.darksky.net/forecast/${darkSkyKey}/${lat},${lng}`;
+    const locationAPI = `${proxy}https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?prox=${lat},${lng}&mode=retrieveAddresses&maxresults=1&app_id=${hereID}&app_code=${hereAppCode}`;
     //Fetch DarkSky API
     fetch(weatherAPI)
-      .then((response) => {
+      .then(response => {
         return response.json();
       })
-      .then((data) => {
+      .then(data => {
         weatherToUser(data);
       });
     //Fetch Here API
     fetch(locationAPI)
-      .then((response) => {
+      .then(response => {
         return response.json();
       })
-      .then((data) => {
-        const address = data.Response.View[0].Result[0].Location.Address
+      .then(data => {
+        const address = data.Response.View[0].Result[0].Location.Address;
         console.log(address);
         locLocation.innerHTML = `${address.District}, ${address.City}`;
       });
-  }
+  };
 
   // Error Function --------------------------------
   //If geolocation is not allowed advert user
   const error = () => {
     noDecision = false;
-    tempDescription.innerHTML = 'For this app work properly, you need to allow your location access!';
-  }
+    tempDescription.innerHTML =
+      'For this app work properly, you need to allow your location access!';
+  };
 
-  const weatherToUser = (data) => {
+  const weatherToUser = data => {
+    const {icon, temperature, precipProbability, precipType} = data.currently;
     console.log(data);
-    temp = data.currently.temperature;
-    temperatureScaleToggler(temp);
-    tempDescription.innerHTML = data.hourly.summary;
-    tempPrecipitation.innerHTML = `${data.currently.precipProbability * 100}% chance of ${data.currently.precipType}`;
-  }
 
+    console.log(icon);
+    setIcon(icon, 'weather-icon');
+    temp = temperature;
+    temperatureScaleToggler(temp);
+    tempDescription.innerHTML = data.hourly.summary; //targeted by hour!
+    tempPrecipitation.innerHTML = `${Math.round(
+      precipProbability * 100,
+    )}% chance of ${precipType}`;
+  };
+
+  function setIcon(icon, iconID) {
+    const skycons = new Skycons({color: '#ededed'});
+    let currentIcon = icon.replace(/-/g, '_').toUpperCase();
+    skycons.play();
+    console.log(currentIcon);
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
   //Toggles scale between C and F
-  
+
   let tempIndex = 1;
 
-  const temperatureScaleToggler = (temp) => {
-    if(tempIndex % 2 == 0) {
+  const temperatureScaleToggler = temp => {
+    if (tempIndex % 2 == 0) {
       const f = temp.toFixed(1);
       //Show current Temperature in Fahrenheit
-      return tempDegree.innerHTML = `${f} F`;
+      return (tempDegree.innerHTML = `${f} F`);
     } else {
-      const c = ((temp - 32) * 5/9).toFixed(1);
+      const c = (((temp - 32) * 5) / 9).toFixed(1);
       //Transforms and show temperature in Celsius
-      return tempDegree.innerHTML = `${c}&#176 C`;
+      return (tempDegree.innerHTML = `${c}&#176 C`);
     }
-  }
+  };
 
   document.addEventListener('click', () => {
     tempIndex++;
     temperatureScaleToggler(temp);
   });
 
-
-
-
   navigator.geolocation.getCurrentPosition(success, error);
-
 }); //___DOMContentLoaded
 //Show current city location
